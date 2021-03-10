a4cc346660855661fe6508d4b701cb1e66b8e923
RichardTech2002 <73807694+RichardTech2002@users.noreply.github.com>
2021-03-10 20:04:32 +1300

    Added more spaces between pages

--- Week4T1.py
+++ Week4T1.py
@@ -1,14 +1,18 @@
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def Write():
-    return '<img scr = LawLogo.png>'
-    return '<h1>Welcome to our law firm!</h1>'
+    return '<!DOCTYPE HTML>\
+        <h1>Welcome to our law firm!</h1>\
+        <img scr = Law_Logo.png>'
+
 
 @app.route('/about')
 def Write_About():
     return '<h1>About Page</h1>'
+
 
 @app.route('/Contact')
 def Write_Contact():
