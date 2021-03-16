7ee95b89a3e9da9ca5740a7ac1ffcfcdbb7ad5b4
Stephan Venter <gs.venter@gmail.com>
2021-03-17 10:35:43 +1300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+"""This is the main flask app file"""
+from flask import Flask, render_template
+import names
+
+app = Flask(__name__)
+
+def generate_names(numberofnames):
+    """Generate random names"""
+    nlist = []
+    for i in range(numberofnames):
+        nlist.append(names.get_full_name())
+        print(i)
+
+    return nlist
+
+namelist = generate_names(10)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Render home page"""
+    return render_template("index.html", namelist=namelist)
+    
